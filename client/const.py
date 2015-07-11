3a20161734df3fd79ba34de9514fa51676b65f4a
tomoki <j-kaneko0115@jcom.home.ne.jp>
2015-07-11 13:25:54 +0900

    サーバー上（監視先）のファイル取得

--- client/const.py
+++ client/const.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 
-remote = ""  # リモートURL
+baseurl  = ''     # サーバー上（監視先） url
+filepath = ''     # サーバー上（監視先） ファイルパス
+username = ''     # basic認証 ID
+password = ''     # basic認証 pass
+
---
+++ client/getupdate.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from urllib import request
+import datetime
+import json
+
+from const import baseurl, filepath, username, password # 設定のインポート
+
+# BASIC認証用の handler を作成する
+# http://docs.python.jp/3.3/howto/urllib2.html
+password_mgr = request.HTTPPasswordMgrWithDefaultRealm()
+password_mgr.add_password(None, baseurl, username, password)
+handler = request.HTTPBasicAuthHandler(password_mgr)
+
+# handler を opener に実装する
+opener = request.build_opener(handler)
+# 作成した opener でリクエストする
+request.install_opener(opener)
+
+# HTTPヘッダーの指定
+req = request.Request(filepath)
+
+# GMTに時刻を変換
+# 1分おきに更新を監視 → 1分前からみて更新のない場合はファイルを取得しない
+# 関数化したい
+now  = datetime.datetime.now()
+diff = datetime.timedelta(hours=-9,minutes=-1)
+adjust = now + diff
+
+udate = adjust.strftime("%a, %d %b %Y %H:%M:%S GMT")
+print(udate)
+req.add_header("If-Modified-Since", udate)
+
+try:
+    response = request.urlopen(req)
+    print(response.info())
+    udata = json.loads(response.read().decode('utf8'))  # byte型から文字列に変換
+    # 更新されていたら一括で変更
+    #print(udata)
+    #print(response.geturl())
+    #print(response.code)
+    #print(response.msg)
+except request.HTTPError as e:
+    # サーバー上のファイルが更新されていない場合 304となり終了
+    # エラー処理は別途検討が必要
+    print(e.reason)
